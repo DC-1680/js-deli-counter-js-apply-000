93f75cd56a8466b05022edcdf00a4a969014974e
Daryl Concha <concha.daryl@gmail.com>
2018-04-02 17:56:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function takeANumber(katzDeliLine, name){
   let currentPosition = katzDeliLine.indexOf(name) + 1;
   return `Welcome, ${name}. You are number ${currentPosition} in line.`
 }
+
+function nowServing(){
+
+  if (katzDeliLine < 0){
+    return 'There is nobody waiting to be served!';
+  }
+  
+  let currentPerson = nowServing[0]; 
+  nowServing.shift(); 
+  return currentPerson; 
+
+}
