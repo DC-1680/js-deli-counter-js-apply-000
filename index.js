d73d9e98bccab9935be4778bc5f9b2544acc5473
Daryl Concha <concha.daryl@gmail.com>
2018-04-02 17:53:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name); 
+  katzDeliLine.push(name);
   let currentPosition = katzDeliLine(name) + 1;
   return `Welcome, ${name}. You are number ${currentPosition} in line.`
 }
